184a139e063b5532d9380c55089ff5bed18817f8
malteasy <aryanmisra4@gmail.com>
2023-11-06 14:01:31 -0500

    power estimation tuned

--- common.py
+++ common.py
@@ -35,10 +35,10 @@ class Checkpoint:
         self.wind_speed = wind_speed
     
     def __str__(self):
-        return f"Lat: {self.lat} | Lon: {self.lon} | Distance: {round(self.distance/1000,1)} \n| Azimuth: {round(self.azimuth,1)} | Elevation: {round(self.elevation,1)} | Cloud Cover: {self.cloud_cover} \n| Wind Dir: {self.wind_dir} | Wind Speed: {self.wind_speed}"
+        return f"Lat: {self.lat} | Lon: {self.lon} | Distance: {round(self.distance,2)} \n| Azimuth: {round(self.azimuth,1)} | Elevation: {round(self.elevation,1)} | Cloud Cover: {self.cloud_cover} \n| Wind Dir: {self.wind_dir} | Wind Speed: {self.wind_speed}"
     
     def __repr__(self):
-        return f"Lat: {self.lat} | Lon: {self.lon} | Distance: {round(self.distance/1000,1)} \n| Azimuth: {round(self.azimuth,1)} | Elevation: {round(self.elevation,1)} | Cloud Cover: {self.cloud_cover} \n| Wind Dir: {self.wind_dir} | Wind Speed: {self.wind_speed}"
+        return f"Lat: {self.lat} | Lon: {self.lon} | Distance: {round(self.distance,2)} \n| Azimuth: {round(self.azimuth,1)} | Elevation: {round(self.elevation,1)} | Cloud Cover: {self.cloud_cover} \n| Wind Dir: {self.wind_dir} | Wind Speed: {self.wind_speed}"
     
 class Route:
     def __init__(self, name: str, checkpoints: list[Checkpoint]):
